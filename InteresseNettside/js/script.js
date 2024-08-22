bd4216b13b6c48083e821b9d9d66120fa71f8f34
filip <filip2007bt@gmail.com>
2024-08-22 10:46:31 +0200

    "Nå har jeg lagt til en onclick funksjon på alle knappene sån at når de trykkes så kommer det en liten tekst generert av AI om hvorfor jeg er interesert i temaet"

--- InteresseNettside/js/script.js
+++ InteresseNettside/js/script.js
@@ -1,1 +1,39 @@
-function
+function popUp() {
+    let text = document.getElementById("gaming");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  } 
+
+
+function popUp2() {  
+    let text = document.getElementById("lydbok");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  function popUp3() {  
+    let text = document.getElementById("manga");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  function popUp4() {  
+    let text = document.getElementById("prog");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+
+
